4eb8b00f145db9d37cc6ab5f26bbcc283577f3a1
skamok <stas_korshankov@tut.by>
2021-03-15 16:02:21 +0300

    fix: refactoring index.js

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-const numbers = document.getElementById('rnd');
-const table = document.getElementById('table');
-
+const numbers = document.getElementById("rnd");
+const table = document.getElementById("table");
 const n = 10; // count of random numbers
-const arr = Array.from({length: n}, () => randn_bm()); // Generate random numbers
 
-const gaussObj = arr.reduce((acc, curr) => { // result object
-  curr in acc ? acc[curr] +=1 : acc[curr] = 1; 
-  return acc;
-}, {});
+const arr = generateArray(n); // Generate random numbers
 
-for (let prop in gaussObj) {
-  console.log(`|${prop}\t| ${gaussObj[prop]}\t|`);
-  const row = document.createElement('tr');
-  const num = document.createElement('td');
-  const count = document.createElement('td');
-  num.innerHTML = prop;
-  count.innerHTML = gaussObj[prop];
-  row.appendChild(num);
-  row.appendChild(count);
-  table.appendChild(row);
-}
+const gaussObj = createGaussObject(); // result object;
 
-numbers.innerHTML = `[${arr}]`
+showNumbers(arr, numbers); // show numbers in html
+fillTable(gaussObj, table); // show numbers distribution in html
 
 function randn_bm() {
   let u = 0;
   let v = 0;
   while (u === 0) u = Math.random();
   while (v === 0) v = Math.random();
-  return Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v) * 2 | 0;
+  return (Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v) * 2) | 0;
+}
+
+function generateArray(n) {
+  return Array.from({ length: n }, () => randn_bm());
+}
+
+function createGaussObject() {
+  return arr.reduce((acc, curr) => {
+    curr in acc ? (acc[curr] += 1) : (acc[curr] = 1);
+    return acc;
+  }, {});
+}
+
+function fillTable(data, parentElement) {
+  for (let prop in data) {
+    console.log(`|${prop}\t| ${data[prop]}\t|`);
+    const row = document.createElement("tr");
+    const num = document.createElement("td");
+    const count = document.createElement("td");
+    num.innerHTML = prop;
+    count.innerHTML = data[prop];
+    row.appendChild(num);
+    row.appendChild(count);
+    parentElement.appendChild(row);
+  }
+}
+
+function showNumbers(data, parentElement) {
+  parentElement.innerHTML = `[${data}]`;
 }
