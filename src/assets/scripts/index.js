ac293eb9836871cc4f8c37f924ae971ed7904b18
skamok <sk_amok@mail.ru>
2021-04-09 10:17:38 +0300

    init: start Tic Tac Toe task

---
+++ src/assets/scripts/index.js
@@ -0,0 +1,14 @@
+import '../styles/reset.css';
+import '../styles/fonts.css';
+import '../styles/iconsfont.css';
+import '../styles/style.scss';
+
+const importImages = {};
+
+function importContext(data, distObj) {
+  data.keys().forEach((key) => {
+    distObj[key] = data(key);
+  });
+}
+
+importContext(require.context('../images', true, /.(png|svg|jpg|jpeg|gif)$/), importImages);
