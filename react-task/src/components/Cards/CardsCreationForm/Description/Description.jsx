a0a61d5693f0ec30399db066c7d6d29944398ff9
skamok <sk_amok@mail.ru>
2021-05-20 09:06:20 +0300

    fix: description component

--- react-task/src/components/Cards/CardsCreationForm/Description/Description.jsx
+++ react-task/src/components/Cards/CardsCreationForm/Description/Description.jsx
@@ -4,6 +4,7 @@ import styles from './Description.module.scss';
 
 function Description({field, inputChange}) {
   const ref = useRef(null);
+  if (field.error) ref.current.focus();
   return (
     <div className={styles.description}>
     <textarea ref={ref} name={field.id} cols="40" rows="3" placeholder={field.placeholder} value={field.value} onChange={inputChange}></textarea>
