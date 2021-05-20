7b4c8007c232f0a8aab03185b8307b93dc68b362
skamok <sk_amok@mail.ru>
2021-05-20 16:26:19 +0300

    fix: es5 deploy

--- es5/index-babel.js
+++ es5/index-babel.js
@@ -1,43 +1,24 @@
 "use strict";
 
-function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+var API_KEY = '65b96a5fe6bc3838312a';
+var baseURL = 'https://free.currconv.com/api/v7/';
+var summaryURL = "currencies?apiKey=".concat(API_KEY); // Example
+// https://free.currconv.com/api/v7/currencies?apiKey=65b96a5fe6bc3838312a
+// https://free.currconv.com/api/v7/convert?q=USD_EUR&compact=ultra&apiKey=65b96a5fe6bc3838312a
 
-function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
-
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && iter[Symbol.iterator] != null || iter["@@iterator"] != null) return Array.from(iter); }
-
-function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-var baseURL = 'https://api.ratesapi.io/api/';
-var summaryURL = 'latest';
-var rateURL = 'latest?base=USD&symbols=GBP';
 var baseElement = document.getElementById('base');
 var symbolElement = document.getElementById('symbol');
 var rateElement = document.getElementById('rate');
 
-var fetchSymbols = function fetchSymbols() {
-  return fetch(baseURL + summaryURL).then(function (response) {
+function fetchData(url) {
+  return fetch(url).then(function (response) {
     if (response.status === 200) {
       return response.json();
     }
 
     throw new Error(response.statusText);
   });
-};
-
-var fetchRate = function fetchRate(base, symbol) {
-  return fetch("".concat(baseURL, "latest?base=").concat(base, "&symbols=").concat(symbol)).then(function (response) {
-    if (response.status === 200) {
-      return response.json();
-    }
-
-    throw new Error(response.statusText);
-  });
-};
+}
 
 var showRate = function showRate() {
   var base = baseElement.options[baseElement.selectedIndex].value;
@@ -46,8 +27,9 @@ var showRate = function showRate() {
   if (base === symbol) {
     rateElement.textContent = 1;
   } else {
-    fetchRate(base, symbol).then(function (data) {
-      var rate = Math.round(Object.values(data.rates)[0] * 100) / 100;
+    var url = "".concat(baseURL, "convert?q=").concat(base, "_").concat(symbol, "&compact=ultra&apiKey=").concat(API_KEY);
+    fetchData(url).then(function (data) {
+      var rate = Math.round(Object.values(data)[0] * 1000) / 1000;
       rateElement.textContent = rate;
     }).catch(function (error) {
       return rateElement.textContent = error;
@@ -67,10 +49,12 @@ var calculate = function calculate() {
   showRate();
 };
 
-fetchSymbols().then(function (data) {
-  var symbols = [data.base].concat(_toConsumableArray(Object.keys(data.rates)));
-  fillDropdawn(symbols, baseElement, 0);
-  fillDropdawn(symbols, symbolElement, 1);
+fetchData(baseURL + summaryURL).then(function (data) {
+  var symbols = Object.keys(data.results);
+  var usdIndex = symbols.indexOf('USD');
+  var eurIndex = symbols.indexOf('EUR');
+  fillDropdawn(symbols, baseElement, usdIndex);
+  fillDropdawn(symbols, symbolElement, eurIndex);
   showRate();
   baseElement.addEventListener('change', calculate);
   symbolElement.addEventListener('change', calculate);
